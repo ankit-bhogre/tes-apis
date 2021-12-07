e7b7782bbc48e1938e40a56deabe21d80676c502
Ankit Bhogre <tns.ankit@gmail.com>
2021-12-07 10:29:23 +0530

    change

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express();
 var cors = require('cors');
 
 const users = require('./src/routes/users');
-const cmsys = require('./src/routes/cms');
-const questions = require('./src/routes/questions');
+/*const questions = require('./src/routes/questions');
 const serviceWorkers = require('./src/routes/serviceWorkers');
 const referrals = require('./src/routes/referral');
 const coupons = require('./src/routes/coupons');
+const cmsys = require('./src/routes/cms');
 const masters = require('./src/routes/masters');
 const feedbacks = require('./src/routes/feedbacks');
-/*const payments = require('./src/routes/payments');
+const payments = require('./src/routes/payments');
 const enablexApi = require('./src/routes/enablexApi');
 const events = require('./src/routes/events');*/
 //const categories = require('./src/routes/categories');
@@ -25,14 +25,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/',users);
-app.use('/cms',cmsys);
-app.use('/questions',questions); //app.use('/questions',verifyJwtToken,questions);
+/*app.use('/questions',questions); //app.use('/questions',verifyJwtToken,questions);
 app.use('/service',serviceWorkers);
 app.use('/referral',referrals);
 app.use('/',coupons);
+app.use('/cms',cmsys);
 app.use('/master',masters);
 app.use('/',feedbacks);
-/*app.use('/payments',payments);
+app.use('/payments',payments);
 app.use('/enablexapi',enablexApi);
 app.use('/',events);*/
 //app.use('/categories',categories);
